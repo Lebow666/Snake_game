db28c74b71633f0b103143c2298f08d14e0d340e
Rom <jeffrey8lebowski@gmail.com>
2023-12-06 22:55:56 +0300

    Revert "add_limit_time"
    This reverts commit 4146980dde6f1bfbe17321cedc9124a4bb1c7d27.

--- js/game.js
+++ js/game.js
@@ -25,28 +25,9 @@ snake[0] = {
 let snakeX = snake[0].x;
 let snakeY = snake[0].y;
 
-document.addEventListener("keydown", startGame);
+document.addEventListener("keydown", direction);
 
 let dir;
-let gameStarted = false;
-let gameTime = 60; // начальное значение времени в секундах
-let gameDuration = 60; // 1 минута
-let remainingTime = gameTime; // оставшееся время
-let timerStarted = false; // флаг для отслеживания запуска таймера
-
-let game;
-
-function startGame(event) {
-  if (!gameStarted) {
-    startTime = Date.now();
-    gameStarted = true;
-    game = setInterval(drawGame, 100);
-    // Добавляем проверку победы только после начала движения змейки
-    setInterval(checkWin, 1000);
-  }
-  direction(event);
-  timerStarted = true; // Устанавливаем флаг в true после начала движения
-}
 
 function direction(event) {
   if (event.keyCode == 37 && dir != "right") dir = "left";
@@ -56,7 +37,7 @@ function direction(event) {
 }
 
 function eatTail(head, arr) {
-  for (let i = 1; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (head.x == arr[i].x && head.y == arr[i].y) clearInterval(game);
   }
 }
@@ -71,7 +52,7 @@ function drawGame() {
   ctx.fillStyle = "white";
   ctx.font = "50px Arial";
   ctx.fillText(score, box * 2.5, box * 1.7);
-  ctx.fillText(remainingTime, box * 15, box * 1.7);
+  ctx.fillText(100, box* 15, box * 1.7); 
 
   if (snakeX == food.x && snakeY == food.y) {
     score++;
@@ -88,12 +69,10 @@ function drawGame() {
     snakeX > box * 17 ||
     snakeY < 3 * box ||
     snakeY > box * 17
-  ) {
+  )
     clearInterval(game);
-    gameOver();
-  }
-
-  if (dir == "left") snakeX -= box;
+  
+if (dir == "left") snakeX -= box;
   if (dir == "right") snakeX += box;
   if (dir == "up") snakeY -= box;
   if (dir == "down") snakeY += box;
@@ -102,50 +81,10 @@ function drawGame() {
     x: snakeX,
     y: snakeY,
   };
-
+  
   eatTail(newHead, snake);
 
   snake.unshift(newHead);
 }
 
-function checkWin() {
-  if (gameStarted && timerStarted && remainingTime > 0) {
-    remainingTime--;
-  } else if (remainingTime <= 0) {
-    clearInterval(game);
-    gameWin();
-  }
-}
-
-function gameOver() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "red";
-  ctx.font = "40px Arial";
-  ctx.fillText("Вы проиграли", box * 6, box * 10);
-  ctx.font = "20px Arial";
-  ctx.fillText("Нажмите Enter, чтобы перезапустить игру", box * 4, box * 12);
-  document.removeEventListener("keydown", direction);
-  document.addEventListener("keydown", restartGame);
-}
-
-function gameWin() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "green";
-  ctx.font = "40px Arial";
-  ctx.fillText("Вы выиграли!", box * 6, box * 10);
-  ctx.font = "20px Arial";
-  ctx.fillText("Нажмите Enter, чтобы перезапустить игру", box * 4, box * 12);
-  document.removeEventListener("keydown", direction);
-  document.addEventListener("keydown", restartGame);
-}
-
-function restartGame(event) {
-  if (event.keyCode == 13) {
-    location.reload();
-  }
-}
-
-// Начать игру
-startGame();
+let game = setInterval(drawGame, 100);
